be0ef20838aea5ce95985bd17fcb6ce25989c227
ArayaEyob <arayacap@icloud.com>
2024-03-18 13:42:52 -0500

    Created the CSV file

--- football_stats.py
+++ football_stats.py
@@ -1,27 +1,30 @@
 from bs4 import BeautifulSoup
-import pandas as pd 
 from io import StringIO
+import pandas as pd
 import requests
 import time
 
 all_teams = []
-url = "https://fbref.com/en/comps/9/Premier-League-Stats"
-html = requests.get(url).text
+html = requests.get("https://fbref.com/en/comps/9/Premier-League-Stats").text
 soup = BeautifulSoup(html, "lxml")
-table = soup.find('table', class_ = 'stats_table')
+table = soup.find_all('table', class_="stats_table")[0]
+links = table.find_all('a')
+links = [l.get("href") for l in links]
+links = [l for l in links if "/squads" in l]
+team_urls = [f"https://fbref.com{l}" for l in links]
 
-for link in table.find_all('a', href=True):
-    if "/squads" in link['href']:
-        team_url = f"https://fbref.com{link['href']}"
-        team_name = link.text.replace(" Stats", "")
-        data = requests.get(team_url).text
-        soup = BeautifulSoup(data, "lxml")
-        stats_table = soup.find('table', class_="stats_table")
-        if stats_table:
-            team_data = pd.read_html(str(stats_table))[0]
-            team_data["Team"] = team_name
-            all_teams.append(team_data)
-        time.sleep(5)
+for team_url in team_urls:
+    team_name = team_url.split("/")[-1].replace("Stats", "")
+    data = requests.get(team_url).text
+    soup = BeautifulSoup(data, "lxml")
+    stats = soup.find_all("table", class_="stats_table")[0]
+    if stats:
+        # Convert the BeautifulSoup tag to a string
+        html_string = str(stats)
+        team_data = pd.read_html(StringIO(html_string))[0]
+        team_data["Team"] = team_name
+        all_teams.append(team_data)
+    time.sleep(5)
 
 stat_df = pd.concat(all_teams)
-stat_df.to_csv("statss.csv", index=False)
+stat_df.to_csv("stats.csv")
