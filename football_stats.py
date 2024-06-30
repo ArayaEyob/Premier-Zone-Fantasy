b2e7a0b7251b83c427086259518a456814cf5525
ArayaEyob <arayacap@icloud.com>
2024-06-29 19:07:58 -0500

    updated App.js

--- football_stats.py
+++ football_stats.py
@@ -8,9 +8,11 @@ all_teams = []
 html = requests.get("https://fbref.com/en/comps/9/Premier-League-Stats").text
 soup = BeautifulSoup(html, "lxml")
 table = soup.find_all('table', class_="stats_table")[0]
+
 links = table.find_all('a')
 links = [l.get("href") for l in links]
 links = [l for l in links if "/squads" in l]
+
 team_urls = [f"https://fbref.com{l}" for l in links]
 
 for team_url in team_urls:
@@ -18,13 +20,16 @@ for team_url in team_urls:
     data = requests.get(team_url).text
     soup = BeautifulSoup(data, "lxml")
     stats = soup.find_all("table", class_="stats_table")[0]
-    if stats:
-        # Convert the BeautifulSoup tag to a string
-        html_string = str(stats)
-        team_data = pd.read_html(StringIO(html_string))[0]
-        team_data["Team"] = team_name
-        all_teams.append(team_data)
+
+    if stats and hasattr(stats, 'columns') and stats.columns:  # Check if 'stats' has 'columns' attribute
+        stats.columns = stats.columns.droplevel()
+
+    # Wrap the HTML string in a StringIO object
+    html_data = StringIO(str(stats))
+    team_data = pd.read_html(html_data)[0]
+    team_data["Team"] = team_name
+    all_teams.append(team_data)
     time.sleep(5)
     
 stat_df = pd.concat(all_teams)
-stat_df.to_csv("stats.csv")
+stat_df.to_csv("stats.csv", index=False)
